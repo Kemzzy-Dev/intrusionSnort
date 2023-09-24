0b1f3fe91c1e7c2a392b844f015276f25a0cfc83
Kemzzy-Dev <kemzzy17@gmail.com>
2023-09-24 07:09:58 +0100

    delay added

--- install.sh
+++ install.sh
@@ -26,7 +26,11 @@ mv resources .resources
 
 sudo python3 installer.py
 
-echo "Done installing Snort...."  
+clear
+
+echo "Installation Successfully Completed"  
+
+sleep 2
 
 sudo python3 snortgui.py
 
